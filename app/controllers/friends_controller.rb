d156131d80c968aa8086536ef4e930d20b69d15e
Navdeep Singh <navdeep5@ualberta.ca>
2024-07-07 14:40:08 -0600

    Added a before_action authenticator to see if logged in or not, and decide if we are allowed to index and show

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -1,5 +1,6 @@
 class FriendsController < ApplicationController
   before_action :set_friend, only: %i[ show edit update destroy ]
+  before_action :authenticate_user!, except: [:index, :show] # If not logged in, only index and show actions are allowed
 
   # GET /friends or /friends.json
   def index
